b5b42c984ab2e60470ca2d76937faf4609711c0b
David Rosa <rosatamsen@gmail.com>
2024-03-10 18:30:22 -0600

    model: Add colors and winning combinations objects.

--- script.js
+++ script.js
@@ -5,6 +5,23 @@
         "null": "",
     }
 
+    const COLORS = {
+        "1": "black",
+        "-1": "black",
+        "null": "white",
+    };
+
+    const  WIN_COMBOS = {
+        "first-row": [0, 1, 2],
+        "mid-row": [3, 4, 5],
+        "last-row": [6, 7, 8],
+        "first-col": [0, 3, 6],
+        "mid-col": [1, 4, 7],
+        "last-col": [2, 5, 8],
+        "forward-diag": [0, 4, 8],
+        "backward-diag": [2, 4, 6]
+    };
+
 
 	/*----- state variables -----*/
     let board;
