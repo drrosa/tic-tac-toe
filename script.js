7ddda89c51f71d83123afb3784a9c0ad32da6c45
David Rosa <rosatamsen@gmail.com>
2024-03-10 18:01:27 -0600

    mode: Create basic setup of application's data.
    - Defines constants, declares state variables,
      and caches DOM elements.

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 	/*----- constants -----*/
+    const MARKERS = {
+        "1": "X",
+        "-1": "O",
+        "null": "",
+    }
 
 
 	/*----- state variables -----*/
+    let board;
+    let winner;
+    let turn;
 
 
 	/*----- cached elements  -----*/
+    const gameStatusEl = document.querySelector("h1");
+    const resetGameButton = document.querySelector("button");
 
 
 	/*----- event listeners -----*/
